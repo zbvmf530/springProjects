8b25e36a3df8828e6acfb4b657753c9d4744c7bd
zbvmf530 <zbvmf530@gmail.com>
2024-07-18 11:03:27 +0900

    파일업로드 ajax 실습

---
+++ boot01/src/main/java/com/yedam/app/upload/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.yedam.app.upload.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	@Value("${file.upload.path}")
+	private String uploadPath;
+	// 경로등록
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry
+			.addResourceHandler("/images/**") //URL
+			.addResourceLocations("file:///"+uploadPath,""); //실제경로
+	}
+
+}
